50fb7fa59f377b2df1f1c9c376271330a6e30184
Jo1910 <35170540+Jo1910@users.noreply.github.com>
2020-12-13 19:18:16 +0000

    Add my first JavaScript Basic Project

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+
+var pet = "This is a string"; //defining a variable and giving it a string value
+window.alert(pet); // writing into an alert box
+
+document.write("She said: \"Let\'s go for a walk!\" "); // writing into the HTML output
+
+document.write("<br><br>"); // 2 lines break
+
+document.write("It was a lovely summer day" + " and I was swimming." ); // concenated string output
+
+document.write("<br><br>"); // 2 lines break
+
+var mouse = "It's a rainy day," + " so I will stay home."; // assigning a concenated string to a variable
+document.write(mouse); // variable output
+
+document.write("<br><br>"); // 2 lines break
+
+var country = "Belgium", river = "Lesse", mountain = "Rodeberg", lake = "Gileppe"; // defining multiple variables
+document.write(river); // one of the variables output
+
+document.write("<br><br>"); // 2 lines break
+
+var mountain = mountain.fontcolor("red"); // Using the fontcolor method on str variable
+document.write(mountain); // variable output
+
+document.write("<br><br>"); // 2 lines break
+
+var river = river.fontcolor("blue"); // Using the fontcolor method on str variable
+document.write(river); // variable output
+
+document.write("<br><br>"); // 2 lines break
+
+var lake = lake.fontcolor("green"); // Using the fontcolor method on str variable
+document.write(lake); // variable output
